4249177fff13f664dfc17be514251865e4683b44
mxfh <mxfh@users.noreply.github.com>
2025-02-05 11:53:14 +0000

    deploy: cismet/carma@288879e80f510d0bf9bcb6fa14826083f850231e

---
+++ store/index.d.ts
@@ -0,0 +1,14 @@
+declare const _default: import('@reduxjs/toolkit').EnhancedStore<{
+    bplaene: {
+        data: undefined;
+        loading: boolean;
+    } & import('redux-persist/es/persistReducer').PersistPartial;
+}, import('redux').UnknownAction, import('@reduxjs/toolkit').Tuple<[import('redux').StoreEnhancer<{
+    dispatch: import('redux-thunk').ThunkDispatch<{
+        bplaene: {
+            data: undefined;
+            loading: boolean;
+        } & import('redux-persist/es/persistReducer').PersistPartial;
+    }, undefined, import('redux').UnknownAction>;
+}>, import('redux').StoreEnhancer]>>;
+export default _default;
