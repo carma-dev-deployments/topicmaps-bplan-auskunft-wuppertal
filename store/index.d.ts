657e9e5cbeb7f87446edd579458e9c1adef21b15
helllth <helllth@users.noreply.github.com>
2025-02-13 17:30:36 +0000

    deploy: cismet/carma@2708739e134066e54fd5a333dd8e58f55d5fd14e

--- store/index.d.ts
+++ store/index.d.ts
@@ -1,14 +1,8 @@
 declare const _default: import('@reduxjs/toolkit').EnhancedStore<{
-    bplaene: {
-        data: undefined;
-        loading: boolean;
-    } & import('redux-persist/es/persistReducer').PersistPartial;
+    bplaene: import('./slices/bplaene').BplaeneState & import('redux-persist/es/persistReducer').PersistPartial;
 }, import('redux').UnknownAction, import('@reduxjs/toolkit').Tuple<[import('redux').StoreEnhancer<{
     dispatch: import('redux-thunk').ThunkDispatch<{
-        bplaene: {
-            data: undefined;
-            loading: boolean;
-        } & import('redux-persist/es/persistReducer').PersistPartial;
+        bplaene: import('./slices/bplaene').BplaeneState & import('redux-persist/es/persistReducer').PersistPartial;
     }, undefined, import('redux').UnknownAction>;
 }>, import('redux').StoreEnhancer]>>;
 export default _default;
