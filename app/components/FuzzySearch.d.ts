3ea355765defd2b5942f5c28a6ada1afab98b681
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2025-03-26 12:19:10 +0000

    deploy: cismet/carma@4c1531201d772da7b9fa75df9d0894eb0a231655

---
+++ app/components/FuzzySearch.d.ts
@@ -0,0 +1,6 @@
+interface FuzzySearchProps {
+    setFeatures: (hit: any) => void;
+    setSelectedIndex: (idx: any) => void;
+}
+declare const FuzzySearch: ({ setFeatures, setSelectedIndex }: FuzzySearchProps) => import("react/jsx-runtime").JSX.Element;
+export default FuzzySearch;
