62f1d960a7ec40109a16633b1f5f196e3659d1e6
helllth <helllth@users.noreply.github.com>
2025-04-15 17:50:49 +0000

    deploy: cismet/carma@9062f181cf86879d4f08619b165724dd80d1921f

--- app/components/FuzzySearchWrapper.d.ts
+++ app/components/FuzzySearchWrapper.d.ts
@@ -2,6 +2,7 @@ interface FuzzySearchProps {
     setFeatures: (hit: any) => void;
     setSelectedIndex: (idx: any) => void;
     onIconClick: () => void;
+    mapSearchAllowed: boolean;
 }
-declare const FuzzySearchWrapper: ({ setFeatures, setSelectedIndex, onIconClick, }: FuzzySearchProps) => import("react/jsx-runtime").JSX.Element;
+declare const FuzzySearchWrapper: ({ setFeatures, setSelectedIndex, onIconClick, mapSearchAllowed, }: FuzzySearchProps) => import("react/jsx-runtime").JSX.Element;
 export default FuzzySearchWrapper;
